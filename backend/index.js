59669bbc8f719891b85da4a60152557ca606cf73
Tom Chambers <tom.chambers@gmail.com>
2021-03-09 17:12:02 +0000

    first commit

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express;
+const port = 3000;
+
+// routes
+
+// controllers
+
+// model
+
+// db
+
+app.listen(port, () => {
+  console.log(`Running on port ${port}`);
+});
